2c7e9fda0d168ba82d9242a29a96ff43d1e423e6
harry07733 <harrymemupal@gmail.com>
2025-02-20 18:04:11 +0000

    faurther code in offline vscode

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const instaId = {
+    userName : "Shaswat Kumar Swain" , 
+    followers : 154 ,
+    following : 177 ,
+    isChat : true , 
+    bio : "I'm from Odisha and I am a coder" , 
+    noOfpost : 1 ,
+    linkBio : "shaswatswain@gmail.com"
+};
+console.table(instaId);
